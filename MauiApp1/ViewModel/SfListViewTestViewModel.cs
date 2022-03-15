4bd28b138c0cd9492bfb7512911fb8b2e3b03089
준 강 <junijuniya@naver.com>
2022-03-15 20:03:22 +0900

    MVVM 코드 추가

--- MauiApp1/ViewModel/SfListViewTestViewModel.cs
+++ MauiApp1/ViewModel/SfListViewTestViewModel.cs
@@ -2,32 +2,45 @@
 
 using System.Collections.ObjectModel;
 
-namespace MauiApp1.ViewModel
+namespace MauiApp1.ViewModel;
+
+public class SfListViewTestViewModel
 {
-    public class SfListViewTestViewModel
+    #region Field
+    private ObservableCollection<BookInfo> bookInfo;
+    #endregion
+
+    #region Properties
+    /// <summary>
+    /// Book 정보
+    /// </summary>
+    public ObservableCollection<BookInfo> BookInfo
     {
-        private ObservableCollection<BookInfo> bookInfo;
-
-        public ObservableCollection<BookInfo> BookInfo
-        {
-            get { return bookInfo; }
-            set { this.bookInfo = value; }
-        }
-
-        public SfListViewTestViewModel()
-        {
-            bookInfo = new ObservableCollection<BookInfo>();
-            bookInfo.Add(new BookInfo() { BookName = "Object-Oriented Programming in C#", BookDescription = "Object-oriented programming is a programming paradigm based on the concept of objects" });
-            bookInfo.Add(new BookInfo() { BookName = "C# Code Contracts", BookDescription = "Code Contracts provide a way to convey code assumptions" });
-            bookInfo.Add(new BookInfo() { BookName = "Machine Learning Using C#", BookDescription = "You will learn several different approaches to applying machine learning." });
-            bookInfo.Add(new BookInfo() { BookName = "Neural Networks Using C#", BookDescription = "Neural networks are an exciting field of software development" });
-            bookInfo.Add(new BookInfo() { BookName = "Visual Studio Code", BookDescription = "It is a powerful tool for editing code and serves for end-to-end programming" });
-            bookInfo.Add(new BookInfo() { BookName = "Visual Studio 2015", BookDescription = "The new version of the widely-used integrated development environment" });
-            bookInfo.Add(new BookInfo() { BookName = "Android Programming", BookDescription = "It provides a useful overview of the Android application life cycle" });
-            bookInfo.Add(new BookInfo() { BookName = "iOS Succinctly", BookDescription = "It is for developers looking to step into frightening world of iPhone" });
-            bookInfo.Add(new BookInfo() { BookName = "Xamarin.Forms", BookDescription = "It creates mappings from its C# classes and controls directly" });
-            bookInfo.Add(new BookInfo() { BookName = "Windows Store Apps", BookDescription = "Windows Store apps present a radical shift in Windows development" });
-            bookInfo.Add(new BookInfo() { BookName = "Machine Learning Using C# 2", BookDescription = "You will learn several different approaches to applying machine learning." });
-        }
+        get { return bookInfo; }
+        set { this.bookInfo = value; }
     }
+    #endregion
+
+    // Constructor
+    #region SfListViewTestViewModel
+    /// <summary>
+    /// SfListViewTestViewModel
+    /// </summary>
+    public SfListViewTestViewModel()
+    {
+        // Set Sample Data
+        bookInfo = new ObservableCollection<BookInfo>();
+        bookInfo.Add(new BookInfo() { BookName = "Object-Oriented Programming in C#", BookDescription = "Object-oriented programming is a programming paradigm based on the concept of objects" });
+        bookInfo.Add(new BookInfo() { BookName = "C# Code Contracts", BookDescription = "Code Contracts provide a way to convey code assumptions" });
+        bookInfo.Add(new BookInfo() { BookName = "Machine Learning Using C#", BookDescription = "You will learn several different approaches to applying machine learning." });
+        bookInfo.Add(new BookInfo() { BookName = "Neural Networks Using C#", BookDescription = "Neural networks are an exciting field of software development" });
+        bookInfo.Add(new BookInfo() { BookName = "Visual Studio Code", BookDescription = "It is a powerful tool for editing code and serves for end-to-end programming" });
+        bookInfo.Add(new BookInfo() { BookName = "Visual Studio 2015", BookDescription = "The new version of the widely-used integrated development environment" });
+        bookInfo.Add(new BookInfo() { BookName = "Android Programming", BookDescription = "It provides a useful overview of the Android application life cycle" });
+        bookInfo.Add(new BookInfo() { BookName = "iOS Succinctly", BookDescription = "It is for developers looking to step into frightening world of iPhone" });
+        bookInfo.Add(new BookInfo() { BookName = "Xamarin.Forms", BookDescription = "It creates mappings from its C# classes and controls directly" });
+        bookInfo.Add(new BookInfo() { BookName = "Windows Store Apps", BookDescription = "Windows Store apps present a radical shift in Windows development" });
+        bookInfo.Add(new BookInfo() { BookName = "Machine Learning Using C# 2", BookDescription = "You will learn several different approaches to applying machine learning." });
+    }
+    #endregion
 }
--- MauiApp1/ViewModel/TestViewModel.cs
+++ MauiApp1/ViewModel/TestViewModel.cs
@@ -1,32 +1,82 @@
 ﻿using System.ComponentModel;
+using System.Windows.Input;
 
-namespace MauiApp1.ViewModel
+namespace MauiApp1.ViewModel;
+
+internal class TestViewModel : INotifyPropertyChanged
 {
-    internal class TestViewModel : INotifyPropertyChanged
+    #region INotifyPropertyChanged
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public void OnPropertyChanged(string propertyName)
     {
-        private string title = "Test Title";
+        this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+    #endregion
 
-        public string Title
+    #region Command
+    public ICommand TestCommand { get; private set; }
+    public ICommand ParameterCommand { get; private set; }
+    #endregion
+
+    #region Field
+    private string title = "MVVM Test";
+    private string paramValue = "";
+    #endregion
+
+    #region Properties
+    /// <summary>
+    /// 타이틀
+    /// </summary>
+    public string Title
+    {
+        get { return this.title; }
+        set
         {
-            get { return title; }
-            set
-            {
-                title = value;
-                OnPropertyChanged("Title");
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public void OnPropertyChanged(string name)
-        {
-            if (this.PropertyChanged != null)
-                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
-        }
-
-        public TestViewModel()
-        {
-            //this.Title = "Test Title";
+            this.title = value;
+            OnPropertyChanged("Title");
         }
     }
+
+    /// <summary>
+    /// 파라미터 값
+    /// </summary>
+    public string ParamValue
+    {
+        get { return this.paramValue; }
+        set
+        {
+            this.paramValue = value;
+            OnPropertyChanged("ParamValue");
+        }
+    }
+    #endregion
+
+    // Constructor
+    #region  TestViewModel
+    /// <summary>
+    /// TestViewModel
+    /// </summary>
+    public TestViewModel()
+    {
+        this.TestCommand = new Command(OnTestCommand);
+        this.ParameterCommand = new Command<string>(OnParameterCommand);
+    }
+    #endregion
+
+    // Methods
+    #region OnTestCommand
+    private void OnTestCommand()
+    {
+        App.Current.MainPage.DisplayAlert("Test", "Test Command Click.", "OK");
+    }
+    #endregion
+
+    #region OnParameterCommand(string param)
+    private void OnParameterCommand(string param)
+    {
+        this.ParamValue = param;
+    }
+    #endregion
 }
+
